27ed812e17fe5f7c23620115232002f83fb131ed
Claudia Fox <claudiafox95@icloud.com>
2018-11-13 16:58:56 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,23 @@ def actor
 end
 
 def monopoly
-	monopoly = {}
+	monopoly = {
+	  :railroads: {
+	    :pieces: 4,
+	    :names: {
+	      :reading_railroad: {
+	        "mortgage_value": "$100",
+	        "mortgage"
+	      }
+	    },
+	    :rent_in_dollars: {
+	      :one_piece_owned: 25,
+	      :two_pieces_owned: 50,
+	      :three_pieces_owned: 100,
+	      :four_pieces_owned: 200
+	    }
+	  }
+	}
 
 end
 
