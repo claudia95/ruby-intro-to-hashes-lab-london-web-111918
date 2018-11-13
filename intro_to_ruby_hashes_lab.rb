8aad617d7c27d72e3b13e5c3d3ea389ed9c20083
Claudia Fox <claudiafox95@icloud.com>
2018-11-13 17:02:06 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,14 +12,10 @@ def monopoly
 	monopoly = {
 	  :railroads: {
 	    :pieces: 4,
-	    :names: { :reading_railroad: { "mortgage_value": "$100",}
+	    :names: { :reading_railroad: { "mortgage_value": "$100"},
 	      :pennsylvania_railroad: { "mortgage_value": "$200" },
-	      :b_and_o_railroad: {
-	        "mortgage_value": "$400"
-	      },
-	      :shortline: {
-	        "mortgage_value": "$800"
-	      }
+	      :b_and_o_railroad: { "mortgage_value": "$400"},
+	      :shortline: {"mortgage_value": "$800"}
 	    },
 	    :rent_in_dollars: {
 	      :one_piece_owned: 25,
